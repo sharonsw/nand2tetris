3eee5f63d4f8e36c5d9df7e5101e7fbea1c7bbf1
Sharon <sharonsuetwli@gmail.com>
2017-08-08 00:55:23 -0700

    Add lesson 3 and 4 projects

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,54 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(INIT)
+@current
+M=0
+
+(LISTEN)
+@KBD
+D=M // Set D to keyboard input value
+@SETBLACK
+D;JNE // Set back if keyboard input
+@SETWHITE
+D;JEQ // Set white if keyboard input = 0
+
+(SETBLACK)
+@current
+M=0
+(LOOP)
+@SCREEN
+D=A // Set D=16384 
+@current 
+A=D+M // Set A to point to RAM[16384 + current]
+M=-1
+@current
+M=M+1 // Increment current count here
+D=M
+@8192 // Last register index
+D=D-A // D = (current - 8192)
+@INIT
+D;JEQ // If finish blackening screen, jump to INIT (reset current count) -> start listening to keyboard inputs again
+@LOOP
+0;JMP
+
+(SETWHITE)
+@current
+D=M // Set D to current word index
+@SCREEN
+A=A+D // Set A to point to RAM[16384 + current]
+M=0 // Set current RAM to 000000..
+@INCR
+0;JMP
+
+(INCR)
+@current 
+M=M+1
+D=M
+@8192 // Last register index
+D=D-A // D = (current - 8192)
+@INIT
+D;JEQ // If current # == 8192 (last register), jump to INIT (reset current count)
+@LISTEN
+0;JMP
+
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@count
+M=0
+@R2
+M=0
+(LOOP)
+@count
+D=M
+@R1
+D=M-D
+@END
+D;JEQ
+@R0
+D=M
+@R2
+M=M+D
+@count
+M=M+1
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
